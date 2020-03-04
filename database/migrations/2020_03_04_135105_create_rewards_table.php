86e8ec450fcf9b22cbdb1371dd034852690af1c2
feedlyy <muhammadnurfadlifadli@gmail.com>
2020-03-04 21:29:07 +0700

    update reward migration

--- database/migrations/2020_03_04_135105_create_rewards_table.php
+++ database/migrations/2020_03_04_135105_create_rewards_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableRewards extends Migration
+class CreateRewardsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -27,6 +27,6 @@ class CreateTableRewards extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_rewards');
+        Schema::dropIfExists('rewards');
     }
 }
